962443399cd38a73829ca555af9c6ce9b7d6ae70
Магомед Чомаев <C:\Users\admin3\AppData\Roaming\The Bat!>
2023-08-14 11:26:32 +0300

    Добавьте файлы проекта.

---
+++ UAT.APP/UAT.APP/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using UAT.APP.Services;
+using UAT.APP.Views;
+
+namespace UAT.APP
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ UAT.APP/UAT.APP/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using UAT.APP.Models;
+
+namespace UAT.APP.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
